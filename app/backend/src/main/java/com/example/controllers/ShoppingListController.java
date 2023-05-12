330742a1cd67d0d2105ede5f9e91f9d62caf01fd
Filip Joksovic <filipjoksovic1@gmail.com>
2023-05-12 18:15:18 +0200

    fix(backend): Rename shoppingLists api mapping

--- app/backend/src/main/java/com/example/controllers/ShoppingListController.java
+++ app/backend/src/main/java/com/example/controllers/ShoppingListController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/shoppinglists")
+@RequestMapping("api/shoppingLists")
 public class ShoppingListController {
     @Autowired
     private ShoppingListService shoppingListService;
