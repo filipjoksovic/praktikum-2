6c103b7f1ef5d29069dac8243042c31af1ab8102
milansoap <milan.seid123@gmail.com>
2023-05-27 20:05:40 +0200

    auth design

--- app/web-app/src/app/app-routing.module.ts
+++ app/web-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AuthComponent},
+  {path: 'auth', component: AuthComponent },
+  {path: '**', component:AuthComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
