968811f05b60146546e49cac55b899238e963660
Filip Joksovic <filipjoksovic1@gmail.com>
2023-05-31 17:43:24 +0200

    chore(web): fix css

--- app/web-app/src/app/components/auth/auth.component.ts
+++ app/web-app/src/app/components/auth/auth.component.ts
@@ -3,27 +3,24 @@ import { OnSameUrlNavigation, Router } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth-service.service';
 
-
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['../../../sass/main.scss']
 })
 export class AuthComponent {
-
   errorMessage: string;
   displayModeString: string;
   isLoginMode: boolean = true;
-  modeButtonString: string = "Login";
-  wantToRegister = "Don't have an account? Click here to register"
-  wantToLogin = "Already have an account? Click here to login "
+  modeButtonString: string = 'Login';
+  wantToRegister = "Don't have an account? Click here to register";
+  wantToLogin = 'Already have an account? Click here to login ';
 
   form: FormGroup;
 
   constructor(private router: Router, private authService: AuthService) {
     this.form = new FormGroup({
-      'email': new FormControl('', [Validators.required, Validators.email]),
-      'password': new FormControl('', Validators.required)
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
     });
   }
 
@@ -32,23 +29,21 @@ export class AuthComponent {
   }
 
   onSwitchMode() {
-    this.isLoginMode = !this.isLoginMode
+    this.isLoginMode = !this.isLoginMode;
     if (this.isLoginMode) {
-      this.displayModeString = this.wantToRegister
-      this.modeButtonString = "Login"
-    }
-    else {
-      this.displayModeString = this.wantToLogin
-      this.modeButtonString = "Register"
+      this.displayModeString = this.wantToRegister;
+      this.modeButtonString = 'Login';
+    } else {
+      this.displayModeString = this.wantToLogin;
+      this.modeButtonString = 'Register';
     }
   }
 
   submit() {
     if (this.isLoginMode) {
-      this.login()
-    }
-    else if (!this.isLoginMode) {
-      this.signUp()
+      this.login();
+    } else if (!this.isLoginMode) {
+      this.signUp();
     }
   }
 
