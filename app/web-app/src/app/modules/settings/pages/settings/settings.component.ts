1e7d68d1d9934e563ef1082b0469d3d63910140f
milansoap <milan.seid123@gmail.com>
2023-06-06 18:15:21 +0200

    adding settings file

--- app/web-app/src/app/modules/settings/pages/settings/settings.component.ts
+++ app/web-app/src/app/modules/settings/pages/settings/settings.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import {
+  faCartShopping,
+  faCodePullRequest,
+  faDatabase,
+  faDoorOpen,
+  faPeopleGroup,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons';
+import { mergeMap, shareReplay } from 'rxjs';
+import { FamilyStoreService } from '../../../services/stores/family-store.service';
+import { FamilyService } from '../../../../services/family.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-settings',
@@ -6,5 +19,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent {
+  protected readonly faPeopleGroup = faPeopleGroup;
 
+  constructor(private familyStore: FamilyStoreService, private familyService: FamilyService,
+              private authService: AuthService    
+    ) {}
+
+
+  public user: User;
+
+  ngOnInit() {
+    this.authService.currentUser$.subscribe(user =>{
+      this.user = user;
+    })
+  }
 }
